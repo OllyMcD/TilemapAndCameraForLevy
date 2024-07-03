2fe3b88868b2af38b4e372f9e2b6f1369cc7786e
S2303831 <S2303831@NDC03196>
2024-07-03 12:47:42 +0100

    Add project files.

---
+++ _Models/Sprite.cs
@@ -0,0 +1,13 @@
+﻿namespace Quickie015;
+
+public class Sprite(Texture2D texture, Vector2 position)
+{
+    private readonly Texture2D _texture = texture;
+    public Vector2 Position { get; protected set; } = position;
+    public Vector2 Origin { get; protected set; } = new(texture.Width / 2, texture.Height / 2);
+
+    public void Draw()
+    {
+        Globals.SpriteBatch.Draw(_texture, Position, null, Color.White, 0f, Origin, 1f, SpriteEffects.None, 0f);
+    }
+}
